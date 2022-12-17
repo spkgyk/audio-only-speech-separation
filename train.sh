de4b1cf42f4d1ac7d7c9c9ed868adbac8d8b9244
spkgyk <spkgyk@gmail.com>
2022-12-17 23:33:26 +0800

    edit train and check configs for errors

--- train.sh
+++ train.sh
@@ -4,4 +4,9 @@
  # @Email: lk21@mails.tsinghua.edu.cn
  # @LastEditTime: 2022-06-02 16:25:26
 ### 
-python -B audio_train.py --conf_dir=configs/dprnn_lrs2_unfolded.yml
+file_list=("audio_train.py --conf_dir=configs/dprnn_lrs2.yml" "audio_train.py --conf_dir=configs/dprnn_wsj0.yml")
+
+for py_file in "${file_list[@]}"
+do
+    python -B ${py_file}
+done
