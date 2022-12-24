b5e16e6227b3a2766e912d6ad0568d0d09d3b6c1
spkgyk <spkgyk@gmail.com>
2022-12-24 23:57:01 +0800

    testing sh file

---
+++ test.sh
@@ -0,0 +1,17 @@
+###
+ # @Author: Kai Li
+ # @Date: 2022-05-27 11:38:30
+ # @Email: lk21@mails.tsinghua.edu.cn
+ # @LastEditTime: 2022-06-02 16:25:26
+### 
+file_list=(
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_baseline_wsj0_2mix_allgpu/conf.yml" 
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_baseline_lrs2_2mix_allgpu/conf.yml"
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_unfolded_wsj0_2mix_allgpu/conf.yml"            
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_unfolded_lrs2_2mix_allgpu/conf.yml"
+            )
+
+for py_file in "${file_list[@]}"
+do
+    python -B ${py_file}
+done
