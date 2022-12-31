c3922a953b262cb4052a04ae0a879e0d139f393b
spkgyk <spkgyk@gmail.com>
2022-12-31 13:54:15 +0800

    tests

--- test.sh
+++ test.sh
@@ -5,10 +5,10 @@
  # @LastEditTime: 2022-06-02 16:25:26
 ### 
 file_list=(
-            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_baseline_wsj0_2mix_allgpu/conf.yml" 
-            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_baseline_lrs2_2mix_allgpu/conf.yml"
-            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_unfolded_wsj0_2mix_allgpu/conf.yml"            
-            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_unfolded_lrs2_2mix_allgpu/conf.yml"
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_baseline_wsj0_2mix_4gpu/conf.yml" 
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_baseline_lrs2_2mix_4gpu/conf.yml"
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_unfolded_wsj0_2mix_4gpu/conf.yml"            
+            "audio_test.py --conf-dir=Experiments/checkpoint/dprnn_unfolded_lrs2_2mix_4gpu/conf.yml"
             )
 
 for py_file in "${file_list[@]}"
